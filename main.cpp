993afdd4cb1ac226b6e50248b8b9dccb20e9e196
b <125986776+unistuff9876@users.noreply.github.com>
2024-06-15 13:19:12 +0700

    форматирование размеров с Kib Mib итд

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <memory>
 #include <iomanip>
+#include <QLocale>
 
 #include "groupstrategy.h"
 #include "groupstrategyfileextensions.h"
@@ -21,17 +22,24 @@ void doTheThing(GroupStrategy* groupStrategy, QString path) {
     //calc-ing spacings
     int namew = 5;
     int sizew = 5;
+
+    static QLocale qLocaleForSizeFormatting;
+    std::vector<std::string> sizeStrings;
+    for (size_t i = 0; i < res.itemCount(); i++) {
+        sizeStrings.push_back(qLocaleForSizeFormatting.formattedDataSize(res.m_sizes[i]).toStdString());
+    }
+
     for (size_t i = 0; i < res.itemCount(); i++) {
         //1 for padding
         namew = std::max(namew, res.m_names[i].size() + 1 + namePrefix.size());
         //1 for log10 + 1 for padding
-        sizew = std::max(sizew, (int)log10(res.m_sizes[i]) + 2);
+        sizew = std::max(sizew, (int)sizeStrings[i].size() + 1);
     }
 
     std::cout << std::setw(namew) << "name" << std::setw(sizew) << "size" << "percentage\n";
     for (size_t i = 0; i < res.itemCount(); i++) {
         std::cout << qPrintable(namePrefix) << std::setw(namew - namePrefix.size()) << qPrintable(res.m_names[i]) \
-                  << std::setw(sizew) << res.m_sizes[i];
+                  << std::setw(sizew) << sizeStrings[i];
 
         float curPercentage = res.m_percentages[i] * 100;
 
@@ -50,16 +58,16 @@ void doTheThing(GroupStrategy* groupStrategy, QString path) {
 
         std::cout << "%\n";
     }
-    std::cout << "\ntotal size: " << res.m_totalSize;
+    std::cout << "\ntotal size: " << qLocaleForSizeFormatting.formattedDataSize(res.m_totalSize).toStdString();
 }
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     //GroupStrategyImmediateFolders
-    /*doTheThing(std::make_shared<GroupStrategyFileExtensions>().get(),
-        "D:/");/**/
     doTheThing(std::make_shared<GroupStrategyImmediateFolders>().get(),
+        "D:/!NESstuff");/**/
+    /*doTheThing(std::make_shared<GroupStrategyImmediateFolders>().get(),
         "D:/Musique/3kliksphilip");/**/
 
     return a.exec();
